3bc3bbeabc2efd1331b90ac4da331216691a4a15
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-03-13 15:56:41 -0400

    Create get_tickers.py

---
+++ get_tickers.py
@@ -0,0 +1,13 @@
+import requests
+
+# rename function
+def get_polygon_tickers():
+    # TODO: url to be the correct query
+    url = f'https://api.polygon.io/v3/reference/tickers?apiKey=a73jrAdWlq0ke5BtQiqYNBZuE8AjZKS2'
+
+    response = requests.get(url)
+    tickers = response.json()
+
+    print(tickers["results"])
+
+    return tickers['results']
