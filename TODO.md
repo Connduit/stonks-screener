e5ee24ba6d3d5bc9ebe89f44abfbcd45571f64bb
Connduit <fuxkvalve@gmail.com>
2025-03-12 22:19:54 -0400

    webpage and python

---
+++ TODO.md
@@ -0,0 +1,19 @@
+# TODO
+
+### Where I Left Off
+
+    - token problem...? failing to push changes 
+
+    - use go, svelte, js, ts, css, html, and wasm files when using live data from a paid api
+
+    - possible sources:
+        https://github.com/kacperadach/ichimoku_screener/
+        https://github.com/pranjal-joshi/Screeni-py/
+        https://github.com/devfinwiz/Stock_Screeners_Raw
+        
+
+    - possible apis:
+        https://site.financialmodelingprep.com/developer/docs/stable
+        https://polygon.io/
+        https://finnhub.io/dashboard
+        
---
+++ getdata.py
@@ -0,0 +1,24 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Define the scope and authenticate using the service account key
+scope = ["https://www.googleapis.com/auth/spreadsheets.readonly", "https://www.googleapis.com/auth/drive.file"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("path/to/your/credentials.json", scope)
+
+# Authenticate and access Google Sheets
+client = gspread.authorize(creds)
+
+# Open the spreadsheet by its name or URL
+spreadsheet = client.open("Stock Data Sheet")
+worksheet = spreadsheet.get_worksheet(0)  # Use the first sheet (index 0)
+
+# Fetch all the data from the sheet
+data = worksheet.get_all_records()
+
+# Convert the data into JSON format (you can use this in your webpage)
+import json
+json_data = json.dumps(data)
+
+# Save the data to a file for use on the webpage
+with open("stock_data.json", "w") as file:
+    file.write(json_data)
