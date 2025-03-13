cddfd7589df037ae5573e456693ba0655bedb97c
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-03-13 16:53:18 -0400

    Update get_tickers.py

--- get_tickers.py
+++ get_tickers.py
@@ -13,6 +13,7 @@ def get_polygon_tickers():
     # TODO: url to be the correct query
     url = f"https://api.polygon.io/v3/reference/tickers?apiKey={apiKey}"
     url = f"https://api.polygon.io/v3/reference/tickers?market=stocks&limit=1000?apiKey={apiKey}"
+    url = f"https://api.polygon.io/v3/reference/tickers?market=stocks&limit=1000?apiKey=a73jrAdWlq0ke5BtQiqYNBZuE8AjZKS2"
 
     response = requests.get(url)
     tickers = response.json()
