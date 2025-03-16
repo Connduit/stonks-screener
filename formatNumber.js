21bdee32c243d2a3e918f8bd2004bf961ece3664
connor <fuxkvalve@gmail.com>
2025-03-16 13:38:20 -0400

    added new js files to implement

---
+++ formatNumber.js
@@ -0,0 +1,17 @@
+// TODO: this function should turn 112.234324 into $112.23 and .2 into $0.20 and etc.
+function toCurrency(number)
+{
+
+}
+
+// TODO: this function should turn 1,234,000 into 1.23 M and etc.
+function formatLargeNumber(number)
+{
+
+}
+
+// TODO: round number: .2f
+function formatRoundNumber(number)
+{
+
+}
---
+++ parseData.js
@@ -0,0 +1,27 @@
+function parseData()
+{
+    fetch('stock_data.json')
+	.then(response => response.json())
+	.then(data => {
+	    const tableBody = document.querySelector('#stock-table tbody');
+
+	    // Iterate through each stock symbol (AAPL, MSFT, GOOG, etc.)
+	    for (let symbol in data) {
+		const stockInfo = data[symbol][0];  // Get the first entry for each stock symbol
+
+		// Create a new row in the table for each stock
+		const tableRow = document.createElement('tr');
+		tableRow.innerHTML = `
+		    <td>${symbol}</td>
+		    <td>${stockInfo.currentPrice}</td>
+		    <td>${stockInfo.currentVolume}</td>
+		    <td>${stockInfo.Gap}</td>
+		    <td>${stockInfo.floatShares}</td>
+		    <td>${stockInfo.relativeVolume}</td>
+		    <td>${stockInfo.News}</td>
+		`;
+		tableBody.appendChild(tableRow);
+	    }
+	})
+	.catch(error => console.error('Error fetching stock data:', error));
+}
