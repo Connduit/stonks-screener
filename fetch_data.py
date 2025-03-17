9123e4b5cb4ab0d4ebca19d237ab79d8aa371630
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-03-17 15:41:22 -0400

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -398,7 +398,7 @@ def getStuff(ticker):
 
 
 # Fetch stock data
-symbols = ["AAPL", "MSFT", "GOOG", "NVDA", "QBTS", "RDUS", "QNTM"] # TODO: fetch all stock symbols from file 
+symbols = ["AAPL", "MSFT", "GOOG", "NVDA", "QBTS", "ANTE"] # TODO: fetch all stock symbols from file 
 data = {} # TODO: this is data for the most active stocks
 
 # https://yfinance-python.org/reference/index.html
