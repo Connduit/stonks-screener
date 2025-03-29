deec09eade71c48b7c9e30a81f39b514efbd475e
Connduit <fuxkvalve@gmail.com>
2025-03-28 21:37:02 -0400

    parse tickers working

--- backend/parseTickers.py
+++ backend/parseTickers.py
@@ -1,60 +1,76 @@
 import os
 import json
+import datetime
 
-print(os.getcwd())
 
-#tickers = {}
-tickers = []
+#def parseTickers(file, symbol, security, marketCategory):
+def parseTickers(filename):
 
-# TODO: async otherlisted.txt
-with open("nasdaqlisted.txt", "r") as file:
-    for line in file:
-        # Symbol|Security Name|Market Category|Test Issue|Financial Status|Round Lot Size|ETF|NextShares
-        [symbol, security_name, market_category, test_issue, financial_status, _, etf, next_shares] = line.strip().split("|")
-        # Market Category: Q = Highest, G = Medium, S = Lowest
-        # Financial Status: N = Normal
-        #print(line)
-        #tickers[symbol] = DataFrame.to_dict(orient="records")
-        if (test_issue == "N"):
-            if not symbol.isalpha():
-                print("'.' in symbol")
-            tickers.append(symbol)
+    try:
+        DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+        DIR_PATH = os.path.join(DIR_PATH, "..", "data")
+    except NameError:
+        DIR_PATH = os.getcwd()
 
-"""
-A = NYSE MKT
-N = New York Stock Exchange (NYSE)
-P = NYSE ARCA
-Z = BATS Global Markets (BATS)
-V = Investors' Exchange, LLC (IEXG)
-"""
-print(len(tickers))
-print(len(list(set(tickers))))
+    tickers = []
 
-count = 0
+    # TODO: async otherlisted.txt?
+    #with open("nasdaqlisted.txt", "r") as file:
+    #with open(filename, "r") as file:
+    with open(f"{DIR_PATH}/{filename}", "r") as file:
+        content = file.read().splitlines()
+        header = content[0].split("|")
+        date = content[-1].split("|")[0][:]
+        date = date[len("File Creation Time: "):]
+        date = datetime.datetime.strptime(date, "%m%d%Y%H:%M")
+        content = content[1:-1]
+        for c in content:
+            [Symbol, Security_Name, Market_Category, Test_Issue, Financial_Status, Round_Lot_Size, ETF, NextShares] = c.split("|")
+            if (Test_Issue == "N" and (Financial_Status == "N" or Financial_Status == "D" or Financial_Status == "E" or Financial_Status == "H")):
+                tickers.append(Symbol)
+                # TODO: more filtering options below...
+                #if "common stock" in Security_Name.lower():
+                #if "depositary" in Security_Name.lower():
+                #if "warrant" in Security_Name.lower():
+                #if Market_Category == "Q" "G" "S"
 
-with open("otherlisted.txt", "r") as file:
-    for line in file:
-        #ACT Symbol|Security Name|Exchange|CQS Symbol|ETF|Round Lot Size|Test Issue|NASDAQ Symbol
-        [act_symbol, security_name, exchange, cqs_symbol, etf, round_lot_size, test_issue, nasdaq_symbol] = line.strip().split("|")
-        # Market Category: Q = Highest, G = Medium, S = Lowest
-        # Financial Status: N = Normal
-        #print(line)
-        #tickers[symbol] = DataFrame.to_dict(orient="records")
-        if (test_issue == "N"):
-            if not act_symbol.isalpha(): # TODO: maybe having "." is okay
-                print(f"act_symbol = {act_symbol}")
-                count += 1
+    """
+    A = NYSE MKT
+    N = New York Stock Exchange (NYSE)
+    P = NYSE ARCA
+    Z = BATS Global Markets (BATS)
+    V = Investors' Exchange, LLC (IEXG)
+    """
+    print(len(tickers))
+    print(len(list(set(tickers))))
 
-            tickers.append(nasdaq_symbol)
+    #with open("otherlisted.txt", "r") as file:
+    with open(f"{DIR_PATH}/otherlisted.txt", "r") as file:
+        content = file.read().splitlines()
+        header = content[0].split("|")
+        date = content[-1].split("|")[0][:]
+        date = date[len("File Creation Time: "):]
+        date = datetime.datetime.strptime(date, "%m%d%Y%H:%M")
+        content = content[1:-1]
 
-print(len(tickers))
-print(len(list(set(tickers))))
-print(f"count = {count}")
+        for c in content:
+            [ACT_Symbol, Security_Name, Exchange, CQS_Symbol, ETF, Round_Lot_Size, Test_Issue, NASDAQ_Symbol] = c.split("|")
+            if (Test_Issue == "N" and ACT_Symbol == CQS_Symbol == NASDAQ_Symbol and (Exchange == "A" or Exchange == "N" or Exchange == "P")):
+                tickers.append(NASDAQ_Symbol)
+                # TODO: more filtering options below...
+                #if "common stock" in Security_Name.lower():
+                #if "depositary" in Security_Name.lower():
+                #if "warrant" in Security_Name.lower():
 
-with open("tickers.json", "w+") as file:
-    json_string = json.dump(tickers, file)
+
+    print(len(tickers))
+    print(len(list(set(tickers))))
+
+    with open(f"{DIR_PATH}/tickers.json", "w+") as file:
+        json.dump(tickers, file)
 
 
 
 if __name__ == "__main__":
-    pass
+    parseTickers("nasdaqlisted.txt")
+   #parseTickers("nasdaqlisted.txt", exchanges="ANP") # NYSE, 
