9778c1f74a32cacdfc3e64fdbf70ac419b920878
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-04-01 14:09:49 -0400

    Update sandbox.py

--- backend/sandbox.py
+++ backend/sandbox.py
@@ -74,7 +74,8 @@ for chunk in chunk_list(all_asset_tickers, 200):
     df_latest_per_ticker = bars.df.groupby(level=0).apply(lambda x: x.xs(x.index.get_level_values(1).max(), level=1))
     filtered_df = df_latest_per_ticker[df_latest_per_ticker['volume'] > 15_000_000]
     #print(df_latest_per_ticker)
-    print(filtered_df)
+    print(filtered_df.index)
+    filtered_tickers.append(filtered_df.index)
 
 
 if __name__ == "__main__":
