085b7a5609e18c8d397f4c240b53213f5a7a53ab
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-04-02 16:25:59 -0400

    Update sandbox.py

--- backend/sandbox.py
+++ backend/sandbox.py
@@ -51,8 +51,8 @@ all_asset_tickers = [asset.symbol for asset in all_assets if (asset.exchange == 
 #print(type(all_assets))
 #print(len(all_assets))
 #print(len(all_asset_tickers)) # length of data/tickers.json is 6680, but get_all_assets after being filtered returns 7940 as length
-print(all_asset_tickers)
-print("DGLY" in all_asset_tickers)
+#print(all_asset_tickers)
+#print("DGLY" in all_asset_tickers)
 
 def chunk_list(tickers, size):
     for i in range(0, len(tickers), size):
@@ -76,7 +76,7 @@ for chunk in chunk_list(all_asset_tickers, 200):
     bars = data_client.get_stock_bars(request_params)
     #print(bars.df.index)
     #print(bars.df.columns) # Index(['open', 'high', 'low', 'close', 'volume', 'trade_count', 'vwap'], dtype='object')
-    print(bars.df)
+    #print(bars.df)
     
     df_latest_per_ticker = bars.df.groupby(level=0).apply(lambda x: x.xs(x.index.get_level_values(1).max(), level=1))
     # df_latest_per_ticker = 
@@ -84,7 +84,6 @@ for chunk in chunk_list(all_asset_tickers, 200):
     #print(bars.df.index.get_level_values("symbol"))
     #print(bars.df.index.get_level_values("timestamp"))
     #print(bars.df.index.get_level_values(1).max()) # gets the most recent datetime to use as an index for each ticker
-    print(bars.df.index.get_level_values("timestamp"))
     
 
     #print(bars.df.index.get_level_values("symbol"))
