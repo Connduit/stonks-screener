ca0a153ecb3e998f40caa933545169a5b752427d
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-04-02 15:28:18 -0400

    Update sandbox.py

--- backend/sandbox.py
+++ backend/sandbox.py
@@ -45,11 +45,11 @@ all_assets = trading_client.get_all_assets(request_params)
 #all_asset_tickers = [asset.symbol for asset in all_assets]
 # TODO: figure out the difference between ARCA and NYSEARCA
 all_asset_tickers = [asset.symbol for asset in all_assets if (asset.exchange == AssetExchange.NASDAQ or asset.exchange == AssetExchange.NYSE or asset.exchange == AssetExchange.AMEX) and asset.tradable == True]
-#print(all_assets)
+print(all_assets)
 #print(type(all_assets))
 #print(len(all_assets))
 #print(len(all_asset_tickers)) # length of data/tickers.json is 6680, but get_all_assets after being filtered returns 7940 as length
-#print(all_asset_tickers)
+print(all_asset_tickers)
 
 def chunk_list(tickers, size):
     for i in range(0, len(tickers), size):
