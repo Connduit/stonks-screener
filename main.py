e20db70d6f8cdef6f0f52e14d93705e5d1cd7c8b
Connor Finch <fuxkvalve@email.com>
2025-04-05 17:41:54 -0400

    outlining historical filters needed

--- main.py
+++ main.py
@@ -15,9 +15,35 @@ def main():
     historical_df = readHistoricalData()
     #historical_df.groupby("symbol")
 
+    # TODO: Apply/compute filters
     #historical_df.groupby("column_name").agg({'col_name1':'mean', 'col_name2':'sum'})
+    #historical_df_groupby = historical_df.groupby("symbol")
     historical_df[f"movAvg_{3}"] = historical_df.groupby("symbol")["close"].transform(lambda df: df.rolling(window=3).mean())
-    print(len(tickers))
+    #historical_df[f"avgVol{3}"] = historical_df.groupby("symbol")["volume"].transform(lambda df: df.rolling(window=3).mean())
+
+    # TODO: FILTERS
+
+    # Gap
+    #   Premarket, active, and post market
+
+    # Change from Close (%)
+    #   Premarket, active, and post market
+
+    # Float
+
+    # Short Interest
+
+    # Relative Volume
+    #   Premarket, active, and post market
+
+    # Relative Volume (5min)
+    #   Premarket, active, and post market
+
+    # (Breaking) News
+
+
+
+    print()
 
 
 if __name__ == "__main__":
